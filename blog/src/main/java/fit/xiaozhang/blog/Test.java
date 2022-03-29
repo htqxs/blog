20c9e9f5f9d03045e4f1d55f4e904e38ca2a3e75
zhangzhi <zhangzhihtz@gmail.com>
2022-03-29 16:40:04 +0800

    init

---
+++ blog/src/main/java/fit/xiaozhang/blog/Test.java
@@ -0,0 +1,25 @@
+package fit.xiaozhang.blog;
+
+/**
+ * @author zhangzhi
+ * @create 2022/2/28 7:24
+ */
+public class Test {
+    public static void main(String[] args) {
+        // Random random = new Random(4);
+        // int[] arr = new int[4];
+        // for (int i = 0; i < 4; i++) {
+        //     arr[i] = random.nextInt(100);
+        // }
+        // // 升序排列
+        // Arrays.sort(arr);
+        // for (int i = 0; i < 4; i++) {
+        //     System.out.println(arr[i]);
+        // }
+        print(1);
+    }
+
+    public static void print(int a) {
+        System.out.println(a);
+    }
+}
